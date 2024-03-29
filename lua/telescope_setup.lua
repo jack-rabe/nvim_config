e5f21dd9249eabc5083c5348b49e6eb9054d97e5
Jack Rabe <jhrabe@wisc.edu>
2024-03-28 19:45:32 -0500

    nicer telescope mapping

--- lua/telescope_setup.lua
+++ lua/telescope_setup.lua
@@ -47,9 +47,6 @@ require('telescope').setup {
     },
   },
   pickers = {
-    command_history = {
-      theme = "ivy"
-    },
     lsp_references = {
       layout_strategy = 'center'
     },
@@ -62,49 +59,12 @@ require('telescope').setup {
 pcall(require('telescope').load_extension, 'fzf')
 pcall(require("telescope").load_extension, "ui-select")
 
--- Telescope live_grep in git root
--- Function to find the git root directory based on the current buffer's path
-local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
-  if current_file == '' then
-    current_dir = cwd
-  else
-    -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ':h')
-  end
-
-  -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
-  if vim.v.shell_error ~= 0 then
-    print 'Not a git repository. Searching on current working directory'
-    return cwd
-  end
-  return git_root
-end
-
--- Custom live_grep function to search in git root
-local function live_grep_git_root()
-  local git_root = find_git_root()
-  if git_root then
-    builtins.live_grep {
-      search_dirs = { git_root },
-    }
-  end
-end
-
-vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
-
 local function telescope_live_grep_open_files()
   builtins.live_grep {
     grep_open_files = true,
     prompt_title = 'Live Grep in Open Files',
   }
 end
-
 
 local nmap = function(keys, func, desc)
   if desc then
@@ -126,14 +86,11 @@ nmap('sf', builtins.find_files, '[S]earch [F]iles')
 nmap('sh', builtins.help_tags, '[S]earch [H]elp')
 nmap('sw', builtins.grep_string, '[S]earch current [W]ord')
 nmap('sg', builtins.live_grep, '[S]earch by [G]rep')
-nmap('sG', ':LiveGrepGitRoot<cr>', '[S]earch by [G]rep on Git Root')
 nmap('sd', builtins.diagnostics, '[S]earch [D]iagnostics')
-nmap('sr', builtins.registers, '[S]earch [R]egisters')
 nmap('ss', builtins.lsp_document_symbols, 'Document [S]ymbols')
 nmap('sc', builtins.command_history, '[C]ommand History')
 nmap('sy', builtins.lsp_dynamic_workspace_symbols, 'Workspace Symbols')
-nmap('su', builtins.resume, '[S]earch Resume')
-nmap('sn', builtins.resume, '[S]earch Resume')
+nmap('sr', builtins.resume, '[S]earch Resume')
 nmap('<space>', builtins.current_buffer_fuzzy_find, 'Current buffer fuzzy find')
 nmap('sH', function() builtins.find_files({ hidden = true }) end, 'Search [H]idden Files')
 nmap('sn', function() require('telescope').extensions.notify.notify({}) end, 'Search [N]otify')
